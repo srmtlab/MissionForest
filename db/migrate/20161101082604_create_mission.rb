2a1b34b2a3a4725c9597981c921c6d7cacd7c67c
Yasuaki Goto <yasu1003@gmail.com>
2016-11-02 04:01:35 +0900

    update

---
+++ db/migrate/20161101082604_create_mission.rb
@@ -0,0 +1,12 @@
+class CreateMission < ActiveRecord::Migration
+  def change
+    create_table :missions do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+    add_index :missions, :user_id
+  end
+end
