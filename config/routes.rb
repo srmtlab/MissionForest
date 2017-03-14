6cce53ae5ee37e89f8a4609ccf11a4a0f87f11ed
Yasuaki Goto <yasu1003@gmail.com>
2017-03-14 16:30:27 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   resources :missions
   resources :tasks
 
-  get 'tasks/:id/new' => 'tasks#new_child', as: :tasks_child_new
-  post 'tasks/:id/create' => 'tasks#create_child', as: :tasks_child_create
-
-  get 'missions/:mission_id/tasks/new' => 'tasks#new', as: :mission_task_new
-  post 'missions/:mission_id/tasks/create' => 'tasks#create', as: :mission_task_create
+  # get 'tasks/:id/new' => 'tasks#new_child', as: :tasks_child_new
+  # post 'tasks/:id/create' => 'tasks#create_child', as: :tasks_child_create
+  # 
+  # get 'missions/:mission_id/tasks/new' => 'tasks#new', as: :mission_task_new
+  # post 'missions/:mission_id/tasks/create' => 'tasks#create', as: :mission_task_create
 
   # api
   get 'api/missions/:id/tasks' => 'missions#show_tasks'
