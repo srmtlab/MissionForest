0e6e22e3dec6f408e3cbead8155d7447df7ca511
sensu-watson <sensunowatson@gmail.com>
2018-03-22 16:28:04 +0900

    bugfix

--- app/models/task.rb
+++ app/models/task.rb
@@ -84,8 +84,8 @@ class Task < ActiveRecord::Base
       prefix mf: <http://lod.srmt.nitech.ac.jp/MissionForest/ontology/>
       prefix xsd: <http://www.w3.org/2001/XMLSchema#>
       prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      INSERT DATA {
-      GRAPH <http://lod.srmt.nitech.ac.jp/MissionForest/>{
+      INSERT DATA 
+      { GRAPH <http://lod.srmt.nitech.ac.jp/MissionForest/> {
       EOS
 
     insertquery += id + ' rdf:type mf:Task ;'
