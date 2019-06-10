6308e7bf97166e89621bfc5e99df0c77b7f2cdc4
akamiya208 <a.kamiya.208@gmail.com>
2019-06-11 07:39:30 +0900

    mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mail setting
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => ENV["G_MAIL_ADDRESS"],
+      :port => 587,
+      :domain => 'smtp.gmail.com',
+      :user_name => ENV["G_MAIL_USERNAME"], #gmailアドレス
+      :password => ENV["G_MAIL_PASSWORD"], #gmailパスワード
+      :authentication => 'login',
+  }
 end
